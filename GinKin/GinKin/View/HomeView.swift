ad04e65932928dae5206671fa5a241c2686264ed
Gregg Abe <greggabe808@gmail.com>
2025-09-01 22:20:30 -1000

    Fixed banner to repeat continously

--- GinKin/GinKin/View/HomeView.swift
+++ GinKin/GinKin/View/HomeView.swift
@@ -8,37 +8,38 @@
 import SwiftUI
 
 struct HomeView: View {
-    
-    let sampleMetals = [
+    @State private var metals: [TempMetalPrice] = [
         TempMetalPrice(symbol: "GOLD", price: 3509.60, change: 0.46),
         TempMetalPrice(symbol: "SILVER", price: 40.95, change: -0.53),
         TempMetalPrice(symbol: "PLATINUM", price: 1429.50, change: 0.48),
         TempMetalPrice(symbol: "PALLADIUM", price: 1163.00, change: -0.74),
         TempMetalPrice(symbol: "RHODIUM", price: 7050.00, change: 2.17)
     ]
-    
+
     var body: some View {
         VStack(spacing: 16) {
-            MetalsPriceBannerView(metalPriceArray: sampleMetals)
+            MetalsPriceBannerView(metalPriceArray: metals)
                 .frame(height: 50)
                 .frame(maxWidth: .infinity)
-                .border(Color.red, width: 2)
-            
+                .border(Color.red, width: 2) // debug border
+
             ScrollView {
-                // net worth view
-                // line graph view
-                // metal prices view
+                // other home content
             }
         }
+        .background(Color("AppBackgroundColor"))
     }
 }
 
-
+#Preview {
+    HomeView()
+        .preferredColorScheme(.dark)
+}
 // tab bar L to R (Home, Stack, Settings
 
-struct HomeView_Previews: PreviewProvider {
-    static var previews: some View {
-        HomeView()
-            .preferredColorScheme(.dark)
-    }
-}
+//struct HomeView_Previews: PreviewProvider {
+//    static var previews: some View {
+//        HomeView()
+//            .preferredColorScheme(.dark)
+//    }
+//}
--- GinKin/GinKin/View/MetalsPriceBannerView.swift
+++ GinKin/GinKin/View/MetalsPriceBannerView.swift
@@ -9,71 +9,75 @@ import SwiftUI
 
 struct MetalsPriceBannerView: View {
     let metalPriceArray: [TempMetalPrice]
+    let spacing: CGFloat = 40
     let speed: CGFloat = 40 // points per second
-    
+
+    @State private var offsetX: CGFloat = 0
     @State private var rowWidth: CGFloat? = nil
-    
+
     var body: some View {
         GeometryReader { geo in
-            TimelineView(.animation) { timeline in
-                let time = CGFloat(timeline.date.timeIntervalSinceReferenceDate)
-                
-                if let rowWidth = rowWidth {
-                    let repeatCount = Int(ceil(geo.size.width / rowWidth)) + 2
-                    
-                    HStack(spacing: 0) {
-                        ForEach(0..<repeatCount, id: \.self) { _ in
-                            HStack(spacing: 40) {
-                                ForEach(metalPriceArray) { metal in
-                                    MetalPriceBanner(metal: metal)
-                                        .fixedSize()
-                                }
-                                Spacer() // <- adds extra space at the end of the row
-                                    .frame(width: 100) // adjust width as desired
-                            
+            if let rowWidth = rowWidth {
+                HStack(spacing: 0) {
+                    // Duplicate the row twice for seamless looping
+                    HStack(spacing: spacing) {
+                        ForEach(metalPriceArray) { metal in
+                            MetalPriceBanner(metal: metal)
+                                .fixedSize()
                         }
+                        
+                        Spacer()
+                    }
+
+                    HStack(spacing: spacing) {
+                        ForEach(metalPriceArray) { metal in
+                            MetalPriceBanner(metal: metal)
+                                .fixedSize()
+                        }
+                        
+                        Spacer()
                     }
                 }
-                    .offset(x: -(time * speed).truncatingRemainder(dividingBy: rowWidth))
-                    .frame(height: 50)
-                    .border(Color.red, width: 2)
-                    .background(Color.black)
+                .offset(x: offsetX)
+                .onAppear {
+                    let duration = Double(rowWidth / speed)
+                    withAnimation(.linear(duration: duration).repeatForever(autoreverses: false)) {
+                        offsetX = -rowWidth
+                    }
+                }
             } else {
-                // temporary single row to measure width
-                HStack(spacing: 40) {
+                // Measure the row width dynamically
+                HStack(spacing: spacing) {
                     ForEach(metalPriceArray) { metal in
                         MetalPriceBanner(metal: metal)
                             .fixedSize()
                     }
+                    
+                    Spacer()
                 }
                 .background(
-                    GeometryReader { geoRow in
+                    GeometryReader { rowGeo in
                         Color.clear
                             .onAppear {
-                                rowWidth = geoRow.size.width
+                                rowWidth = rowGeo.size.width
+                                offsetX = 0
                             }
                     }
                 )
             }
         }
-    }
         .frame(height: 50)
-}
+        .clipped()
+    }
 }
 
 #Preview {
-    ScrollView(.horizontal, showsIndicators: false) {
-        HStack(spacing: 40) {
-            MetalPriceBanner(metal: TempMetalPrice(symbol: "GOLD", price: 3509.60, change: 0.46))
-            MetalPriceBanner(metal: TempMetalPrice(symbol: "SILVER", price: 40.95, change: -0.53))
-            MetalPriceBanner(metal: TempMetalPrice(symbol: "PLATINUM", price: 1429.50, change: 0.48))
-            MetalPriceBanner(metal: TempMetalPrice(symbol: "PALLADIUM", price: 1163.00, change: -0.74))
-            MetalPriceBanner(metal: TempMetalPrice(symbol: "RHODIUM", price: 7050.00, change: 2.17))
-        }
-        .padding()
-        .frame(height: 50)
-        .background(Color("AppBackgroundColor"))
-        .border(Color.red, width: 2)
-    }
+    MetalsPriceBannerView(metalPriceArray: [
+        TempMetalPrice(symbol: "GOLD", price: 3509.60, change: 0.46),
+        TempMetalPrice(symbol: "SILVER", price: 40.95, change: -0.53),
+        TempMetalPrice(symbol: "PLATINUM", price: 1429.50, change: 0.48),
+        TempMetalPrice(symbol: "PALLADIUM", price: 1163.00, change: -0.74),
+        TempMetalPrice(symbol: "RHODIUM", price: 7050.00, change: 2.17)
+    ])
     .preferredColorScheme(.dark)
 }
