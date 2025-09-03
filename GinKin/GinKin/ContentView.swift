0e40eea84f82ad1d16e19c776c9954f5e01fd65c
Gregg Abe <greggabe808@gmail.com>
2025-09-02 23:32:24 -1000

    Added tab bar to content view

--- GinKin/GinKin/ContentView.swift
+++ GinKin/GinKin/ContentView.swift
@@ -7,8 +7,38 @@
 
 import SwiftUI
 
+
 struct ContentView: View {
     var body: some View {
-        HomeView()
+        TabView {
+            NavigationStack {
+                HomeView()
+            }
+            .tabItem {
+                Image(systemName: "house")
+                Text("Home")
+            }
+            .tag(0)
+
+            // Search Tab
+            NavigationStack {
+                EmptyView()
+            }
+            .tabItem {
+                Image(systemName: "dollarsign.circle")
+                Text("Stack")
+            }
+            .tag(1)
+
+            // Profile Tab
+            NavigationStack {
+                EmptyView()
+            }
+            .tabItem {
+                Image(systemName: "gear")
+                Text("Settings")
+            }
+            .tag(2)
+        }
     }
 }
